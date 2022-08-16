dbd6bca30a4f924672014e2f141079ad8758399b
Soapey <granttravissoper@gmail.com>
2022-08-16 20:04:28 +1000

    -a

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import './Board.css';
+import React from "react";
+import "./Board.css";
+import Tile from "../Tile";
 
 const Board = (props) => {
-
   const gridConfig = {
     gridTemplateColumns: `repeat(${Math.sqrt(props.numTiles)}, 1fr)`,
     gridTemplateRows: `repeat(${Math.sqrt(props.numTiles)}, 1fr)`,
-  }
+  };
 
+  const tileObjectArray = tiles.map((tile) => {
+    return <Tile tile={...tile} />;
+  });
 
-  return (
-    <div className='Board' style={gridConfig}>
-    </div>
-  )
-}
+  return <div className="Board" style={gridConfig}>{tileObjectArray}</div>;
+};
 
-export default Board
+export default Board;
