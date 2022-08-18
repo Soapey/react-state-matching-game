b2f35c330adc943fda02b56e947dab64da76c75b
Soapey <granttravissoper@gmail.com>
2022-08-18 18:26:07 +1000

    -a

--- src/components/TileSelector/TileSelector.js
+++ src/components/TileSelector/TileSelector.js
@@ -5,7 +5,7 @@ import { useHover } from "../../hooks";
 const TileSelector = (props) => {
   const [ref, hovered] = useHover();
 
-  const dropdown = (
+  const dropdown = hovered ? (
     <div className="tileSelectorContent">
       <div
         className="number"
@@ -32,14 +32,14 @@ const TileSelector = (props) => {
         36
       </div>
     </div>
-  );
+  ) : null;
 
   return (
     <div className="tileSelector">
       <div>Number of Tiles</div>
       <div className="tileSelectorDropdown" ref={ref}>
         {props.numTiles}
-        {hovered ? dropdown : null}
+        {dropdown}
       </div>
     </div>
   );
