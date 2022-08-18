6f2f53f8d1d920a98b9198024a2c2c78446cf779
Soapey <granttravissoper@gmail.com>
2022-08-18 18:17:51 +1000

    -a

--- src/hooks.js
+++ src/hooks.js
@@ -4,22 +4,17 @@ export default function useHover() {
   const ref = useRef();
   const [hovered, setHovered] = useState(false);
 
-  enter = () => {
-    setHovered(true);
-  };
-
-  leave = () => {
-    setHovered(false);
-  };
+  enter = () => setHovered(true);
+  leave = () => setHovered(false);
 
   useEffect(() => {
     const refCopy = ref;
-    refCopy.current.addEventListener("mouseenter", enter());
-    refCopy.current.addEventListener("mouseleave", leave());
+    refCopy.current.addEventListener("mouseenter", enter);
+    refCopy.current.addEventListener("mouseleave", leave);
 
     return () => {
-      refCopy.current.removeEventListener("mouseenter", enter());
-      refCopy.current.removeEventListener("mouseleave", leave());
+      refCopy.current.removeEventListener("mouseenter", enter);
+      refCopy.current.removeEventListener("mouseleave", leave);
     };
   });
 
